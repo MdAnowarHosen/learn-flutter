9ab29c9c9baeb682efc1e8b71f465aa0b2148555
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-03-20 18:58:10 +0600

    my app

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeActivity());
+    return MaterialApp(home: HomeActivity(),
+    // theme: ThemeData(primarySwatch: Colors.green) ,
+    );
   }
-
 }
 
-class HomeActivity extends StatelessWidget{
+class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('My App'), backgroundColor: Colors.green,),
-      body: Text('Bismillah'),
-
+      appBar: AppBar(
+        title: const Text('My App'),
+        backgroundColor: Colors.green,
+      ),
+      body: Text('Bismillah, starting by the name of Allah'),
     );
   }
-
 }
