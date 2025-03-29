5a7306930b464fc9916f3b52aa372d1518ec9cac
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-03-29 15:28:19 +0600

    navbar design

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: HomeActivity(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.green),
       // theme: ThemeData(primarySwatch: Colors.green) ,
       // color: Colors.green,
     );
@@ -29,72 +30,39 @@ class HomeActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Barta'),
-        backgroundColor: Colors.amber,
+        title: Text("Flutter App"),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        elevation: 8.0,
+        shadowColor: Colors.green[900],
+        surfaceTintColor: Colors.green[900],
         actions: [
           IconButton(
-            onPressed: () {
-              MySnackBar('I am message', context);
-            },
-            icon: Icon(Icons.message),
-          ),
-          IconButton(
-            onPressed: () {
-              MySnackBar('I am notifications', context);
-            },
-            icon: Icon(Icons.notifications),
-          ),
-          IconButton(
-            onPressed: () {
-              MySnackBar('I am search', context);
-            },
-            icon: Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {
-              MySnackBar('I am user', context);
-            },
-            icon: Icon(Icons.person),
-          ),
-        ],
-      ),
-      body: Center(child: Text('Bismillah')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          MySnackBar('I am  Floating action button', context);
-        },
-        elevation: 10,
-        child: Icon(Icons.add),
-        backgroundColor: Colors.amber,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        onTap: (int index) {
-          if (index == 0) {
-            MySnackBar('I am search bottom menu', context);
-          } else if (index == 1) {
-            MySnackBar('I am home bottom menu', context);
-          } else if (index == 2) {
-            MySnackBar('I am profile bottom menu', context);
-          }
-        },
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(child: Text('Barta')),
-            ListTile( leading: Icon(Icons.home), title: Text('Home'),),
-            ListTile(leading: Icon(Icons.post_add), title: Text('Blog'),),
-            ListTile(leading: Icon(Icons.contact_emergency), title: Text('Contact'),),
-            ListTile(leading: Icon(Icons.settings), title: Text('Settings'),),
-            ListTile(leading: Icon(Icons.info), title: Text('About'),),
-          ],
+              onPressed: () {
+                MySnackBar("Search", context);
+              },
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
         ),
+          IconButton(
+              onPressed: () {
+                MySnackBar("Settings", context);
+              },
+              icon: Icon(Icons.settings),
+              tooltip: 'Settings',
+          ),
+          IconButton(
+              onPressed: () {
+                MySnackBar("Profile", context);
+              },
+              icon: Icon(Icons.person),
+              tooltip: 'Profile',
+          )
+          ],
+
+      ),
+      body: Center(
+        child: Text("Hello Flutter"),
       ),
     );
   }
