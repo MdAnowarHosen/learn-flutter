7feec9e2da44725b20b447ca8690543fb2c09f64
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-04-22 17:02:06 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       home: HomeActivity(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
       // theme: ThemeData(primarySwatch: Colors.green) ,
       // color: Colors.green,
@@ -99,11 +99,10 @@ class HomeActivity extends StatelessWidget {
                 accountEmail: Text('anowarhosensoft@gmail.com'),
                 currentAccountPicture: CircleAvatar(
                   backgroundImage: NetworkImage(
-                      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                    'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                  ),
                 ),
-                onDetailsPressed: () => {
-                  MySnackBar("Profile", context)
-                },
+                onDetailsPressed: () => {MySnackBar("Profile", context)},
               ),
             ),
             ListTile(
@@ -145,7 +144,9 @@ class HomeActivity extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(child: Text("Hello Flutter")),
+      body: Center(
+        child: CircularProgressIndicator(color: Colors.green, strokeWidth: 5.0),
+      ),
     );
   }
 }
